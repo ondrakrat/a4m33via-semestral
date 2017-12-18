6f0fdc96c027a2d263b3f025a4813aa1a2915e34
Ondra Kratochvíl <ondrej.kratochvil@keyup.eu>
2017-12-18 23:48:25 +0100

    Load current position

--- public/javascripts/geolocation.js
+++ public/javascripts/geolocation.js
@@ -1,25 +1,44 @@
 /**
  * Created by Ondřej Kratochvíl on 18.12.17.
  */
-export function getLocation() {
-    if (window.navigator.geolocation) {
-        window.navigator.geolocation.getCurrentPosition(
-            position => {
-                const latitude = position.coords.latitude;
-                const longitude = position.coords.longitude;
-                return {
-                    latitude: latitude,
-                    longitude: longitude
-                };
-            },
-            _errorCallback,
-            {maximumAge: 600000, timeout: 5000, enableHighAccuracy: true}
-        )
-    } else {
-        console.error("Geolocation API is not supported in your browser.");
-    }
-}
+class Geolocation {
 
-function _errorCallback(error) {
-    console.error("Error occurred when retrieving current position", error);
+    static getLocation(callback) {
+        if (window.navigator.geolocation) {
+            window.navigator.geolocation.getCurrentPosition(
+                position => {
+                    const latitude = position.coords.latitude;
+                    const longitude = position.coords.longitude;
+                    this.coords = {
+                        latitude: latitude,
+                        longitude: longitude
+                    };
+                    if (!!callback) {
+                        callback(this);
+                    }
+                },
+                this._errorCallback,
+                {maximumAge: 600000, timeout: 5000, enableHighAccuracy: true}
+            );
+        } else {
+            console.error("Geolocation API is not supported in your browser.");
+        }
+    }
+
+    _errorCallback(error) {
+        switch (error.code) {
+            case error.PERMISSION_DENIED:
+                console.error("User denied the request for Geolocation.", error);
+                break;
+            case error.POSITION_UNAVAILABLE:
+                console.error("Location information is unavailable.", error);
+                break;
+            case error.TIMEOUT:
+                console.error("The request to get user location timed out.", error);
+                break;
+            case error.UNKNOWN_ERROR:
+                console.error("An unknown error occurred.", error);
+                break;
+        }
+    }
 }
